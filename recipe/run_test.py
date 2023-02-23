851ca8817fe775af2ebeb8a8d754f467b1627d51
Charles Bousseau <cbousseau@anaconda.com>
2023-02-23 15:03:52 -0500

    make linter happy

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-if __name__ == '__main__':
-    import sys
-    import pytest
 
+import cvxopt
+import cvxopt.base
+import cvxopt.blas
+import cvxopt.cholmod
+import cvxopt.lapack
+import cvxopt.misc_solvers
+import cvxopt.umfpack
+import cvxopt.glpk
+import cvxopt.fftw
+
+import sys
+import pytest
+
+if __name__ == '__main__':
     sys.exit(pytest.main())
